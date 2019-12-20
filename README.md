b6a9c826127620446388ee5e7d53ea83bcc68af3
iknox <iknox@narrativescience.com>
2019-12-20 14:56:23 -0600

    Updated readme and results page

--- README.md
+++ README.md
@@ -2,15 +2,14 @@
 ## (a toy implementation)
 ### Setup
 
-* Log in to salesforce and configure a new connected app (@TODO provide tutorial link).
+* Log in to Salesforce and create a [new connected app](https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/intro_defining_remote_access_applications.htm).
 
 Within your virtual environment of choice...
 * Install the requirements: `pip install -r requirements.txt -r requirements-dev.txt`
-* Configure the app `cp .env-example .env && vi .env` 
-    (must provide minimally a Client ID and Client Secret from SF)
+* Configure the app `cp .env-example .env && vi .env` (must provide minimally a Client ID and Client Secret from SF)
 
 ### Running the toy
-`./start`
-Point your browser to `http://localhost:8000` and follow the provided link!
+* Start the local web server `./start`
+* Point your browser to `http://localhost:8000` and follow the provided link!
 
 @TODO: Actually query the API to prove it works.
--- src/resources/OAuthCallback.py
+++ src/resources/OAuthCallback.py
@@ -1,5 +1,6 @@
 import requests
-import json
+
+from falcon_jinja2 import FalconTemplate
 from os import getenv
 from urllib.parse import urlunparse
 
@@ -9,8 +10,10 @@ class OAuthCallbackResource(object):
     Handles the callback from the IDP
     """
 
-    @staticmethod
-    def on_get(req, resp):
+    ft = FalconTemplate()
+
+    @ft.render('/callback/index.html')
+    def on_get(_, req, resp):
         # build token url
         token_uri = urlunparse([
             getenv('OAUTH_URI_SCHEME'),
@@ -30,7 +33,12 @@ class OAuthCallbackResource(object):
             'code': req.get_param('code')
         }
 
-        # exchange code for access token
-        token_response = requests.post(token_uri, data=payload)
+        # exchange code for access
+        token_response = requests.post(token_uri, data=payload).json()
 
-        resp.body = json.dumps(token_response.json())
+        # set the context to use in the template
+        resp.context = token_response
+        resp.context.update({
+            'state': req.get_param('state'),
+            'code': req.get_param('code')
+        })
